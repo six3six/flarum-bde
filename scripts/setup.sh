b80017c8f4233609b2f904c7a656309cfe3d7c9b
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2019-07-24 01:11:02 +0200

    Add french extension

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@ mkdir ../db
 
 docker-compose up -d
 docker-compose exec web bash -c "cd /var/www/html && composer create-project flarum/flarum . --stability=beta && chmod 777 . -R"
-
+docker-compose exec web bash -c "cd /var/www/html && composer require milescellar/lang-french"
 
