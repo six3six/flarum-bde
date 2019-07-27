4497bb1c4a2aea628168f22e2203c12564224591
Louis Desplanche <louis.desplanche@edu.esiee.fr>
2019-07-27 19:05:08 +0200

    Fix execution error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-rm ../data
-rm ../db
+
 mkdir ../data
 mkdir ../db
 
 docker-compose up -d
-docker-compose exec web bash -c "cd /var/www/html && composer create-project flarum/flarum . --stability=beta && chmod 777 . -R"
+if [[ -z "$(ls -A ../data)" ]]; then
+   docker-compose exec web bash -c "cd /var/www/html && composer create-project flarum/flarum . --stability=beta && chmod 777 . -R"
+fi
+
 cp ./install_requirements.sh ../data/
+chmod +x ../data/install_requirements.sh
 docker-compose exec web bash -c "cd /var/www/html && ./install_requirements.sh"
 
