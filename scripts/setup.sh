5029aa2d3cc0f007c6553e953698fd155cd89751
Louis Desplanche <louis.desplanche@edu.esiee.fr>
2019-07-27 19:37:58 +0200

    Add verifs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-mkdir ../data
-mkdir ../db
+docker-compose down
+if [[ -z "$(ls -A ../data)" ]]; then
+    read -r -p "Wipe data ? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        rm ../data -rf
+    fi
+else
+    mkdir ../data
+fi
+
+if [[ -z "$(ls -A ../db)" ]]; then
+    read -r -p "Wipe db ? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        rm ../db -rf
+    fi
+else
+    mkdir ../db
+fi
 
 docker-compose up -d
 if [[ -z "$(ls -A ../data)" ]]; then
    docker-compose exec web bash -c "cd /var/www/html && composer create-project flarum/flarum . --stability=beta && chmod 777 . -R"
 fi
 
-cp ./install_requirements.sh ../data/
-chmod +x ../data/install_requirements.sh
+cp ./install_requirements.sh ../data/ && chmod +x ../data/install_requirements.sh
 docker-compose exec web bash -c "cd /var/www/html && ./install_requirements.sh"
 
